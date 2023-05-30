57a5234674c8b4fccb93473090a4298aba139669
Gabriella <gabriellamargarida18@gmail.com>
2023-05-30 19:36:39 -0300

    adiciona atualização no carregamento

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const form = document.forms.entrada;
         form.addEventListener('submit', envia);
 let estado = 0;
-        function envia(evento){
-            evento.preventDefault ();
-            console.log('Formulario enviado!');
-            estado++;
-            atualiza();
+
+atualiza();
+    function envia(evento){
+        evento.preventDefault ();
+        console.log('Formulario enviado!');
+        estado++;
+        atualiza();
         }
 
         function atualiza(){
